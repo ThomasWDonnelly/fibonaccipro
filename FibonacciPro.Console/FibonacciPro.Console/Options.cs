ed8843ff6ff6c18626ff948c02e0af2aeac0341b
Ken Sykora <ksykora@nerdery.com>
2013-12-13 15:10:57 -0600

    Initial logic for interactive mode

---
+++ FibonacciPro.Console/FibonacciPro.Console/Options.cs
@@ -0,0 +1,32 @@
+﻿using CommandLine;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FibonacciPro.ConsoleApplication
+{
+    /// <summary>
+    /// Options to be passed into the application from the command line
+    /// </summary>
+    class Options
+    {
+        [Option('n',"interactive", MutuallyExclusiveSet="input", HelpText="Enables interactive mode where the user will be prompted for input values.", DefaultValue=true)]
+        public bool InteractiveMode { get; set; }
+
+        [Option('i',"in", MutuallyExclusiveSet="input", HelpText="File path to input file. XML or plain text accepted.")]
+        public string InputFile { get; set; }
+
+        public enum FileType { PlainText, Xml }
+
+        [Option('o',"out",HelpText="File path to output file. Files ending in .xml will be an XML format.")]
+        public string OutputFile { get; set; }
+
+        public FileType OutputFileType { get; set; }
+
+        public FileType InputFileType { get; set; }
+
+        public int InputNumber { get; set; }
+    }
+}
---
+++ FibonacciPro.Console/FibonacciPro.Console/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FibonacciPro.ConsoleApplication
+{
+    class Program
+    {
+        private static Options _options;
+
+        static void Main(string[] args)
+        {
+            _options = ParseOptions(args);
+
+            if (_options.InteractiveMode)
+            {
+                Interact();
+            }
+        }
+
+        private static void Interact()
+        {
+            var result = 0;
+
+            do
+            {
+                Console.Write("Please enter the number of sequences to calculate: ");
+                var input = Console.ReadLine();
+                int.TryParse(input, out result);
+
+            } while (result == 0);
+
+            _options.InputNumber = result;
+        }
+
+        private static Options ParseOptions(string[] args) {
+            var options = new Options();
+            CommandLine.Parser.Default.ParseArguments(args, options);
+
+            return options;
+        }
+    }
+}
