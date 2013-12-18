9a23a747941063df06806baf950a6b2f4f50fac4
unknown <araddatz@araddatz-win8.nerdery.ad.bravo>
2013-12-18 15:22:24 -0600

    started MVC project, made a few basic edits

---
+++ Fibonacci.Web/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Fibonacci.Web.Startup))]
+namespace Fibonacci.Web
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
