231c2be825c0ad9c06905d08e132fccf2fc04e10
Ken Sykora <ksykora@nerdery.com>
2013-12-20 10:43:17 -0600

    Added unit tests for XML Input Refactored ConsoleIOHandler to take generic TextReader/Writer paramater to support other potential stream readers/writers

--- FibonacciPro.Console/IO/ConsoleIOHandler.cs
+++ FibonacciPro.Console/IO/ConsoleIOHandler.cs
@@ -5,26 +5,8 @@ using System.Numerics;
 
 namespace FibonacciPro.ConsoleApplication.IO
 {
-    public class ConsoleIOHandler : IInputHandler, IOutputHandler
+    public class ConsoleIOHandler : TextStreamIOHandler
     {
-        public int GetNumber()
-        {
-            var result = 0;
-
-            do
-            {
-                Console.Write("Please enter the number of sequences to calculate: ");
-                var input = Console.ReadLine();
-                int.TryParse(input, out result);
-
-            } while (result == 0);
-
-            return result;
-        }
-
-        public void Write(IEnumerable<BigInteger> results)
-        {
-            Console.Write(string.Join(" ", results.Select(r => r.ToString("R0"))));
-        }
+        public ConsoleIOHandler() : base(Console.In, Console.Out) { }
     }
 }
---
+++ FibonacciPro.Tests/XmlIOHandlerTests.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using FibonacciPro.ConsoleApplication.IO;
+using System.IO;
+
+namespace FibonacciPro.Tests
+{
+    [TestClass]
+    public class XmlIOHandlerTests
+    {
+        [TestMethod]
+        public void handler_can_accept_xml_input()
+        {
+
+            //Arrange
+            var handler = new XmlIOHandler("input.xml");
+            var expectedResult = 22;
+
+            //Act
+            var result = handler.GetNumber();
+
+            //Assert
+            Assert.AreEqual(expectedResult, result);
+        }
+
+        [TestMethod]
+        public void handler_throws_argument_exception_when_given_invalid_path()
+        {
+
+            //Arrange
+            var handler = new XmlIOHandler("does-not-exist.xml");
+
+            //Act
+            try {
+                var result = handler.GetNumber();
+                Assert.Fail("did not throw ArgumentException with path paramater");
+            }
+
+            //Assert
+            catch (ArgumentException ex) {
+                Assert.AreEqual("path", ex.ParamName);
+            }
+        }
+
+        [TestMethod]
+        public void handler_throws_application_exception_when_given_invalid_markup()
+        {
+
+            //Arrange
+            var path = "invalid-markup-input.xml";
+            var handler = new XmlIOHandler(path);
+            if (!File.Exists(path))
+            {
+                Assert.Inconclusive("invalid-markup-input.xml file was not present to test with");
+            }
+            //Act
+            try
+            {
+                var result = handler.GetNumber();
+                Assert.Fail("did not throw ArgumentException with path paramater");
+            }
+
+            //Assert
+            catch (ApplicationException ex)
+            {
+                Assert.IsTrue(true); //Pass!
+            }
+        }
+
+        [TestMethod]
+        public void handler_throws_application_exception_when_given_invalid_schema()
+        {
+
+            //Arrange
+            var path = "invalid-schema-elements-input.xml";
+            var handler = new XmlIOHandler(path);
+            if (!File.Exists(path))
+            {
+                Assert.Inconclusive("invalid-schema-elements-input.xml file was not present to test with");
+            }
+            //Act
+            try
+            {
+                var result = handler.GetNumber();
+                Assert.Fail("did not throw ArgumentException with path paramater");
+            }
+
+            //Assert
+            catch (ArgumentException ex)
+            {
+                Assert.AreEqual("path", ex.ParamName);
+            }
+        }
+    }
+}
