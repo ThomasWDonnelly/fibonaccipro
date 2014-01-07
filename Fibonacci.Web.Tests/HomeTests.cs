bfed21c8b6391a53ad0112b1d79b65d92456cdb8
Ken Sykora <ksykora@nerdery.com>
2014-01-07 10:49:03 -0600

    Added initial unit tests for web project

---
+++ Fibonacci.Web.Tests/HomeTests.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Fibonacci.Web.Controllers;
+using MvcContrib.TestHelper;
+
+namespace Fibonacci.Web.Tests
+{
+    [TestClass]
+    public class HomeTests
+    {
+
+        [TestInitialize]
+        public void Setup()
+        {
+            
+        }
+
+        [TestMethod]
+        public void Index_returns_index_view()
+        {
+            //Arrange
+            var controller = new HomeController(null);
+
+            //Act
+            var result = controller.Index();
+
+            //Assert
+            result.AssertViewRendered();
+        }
+    }
+}
